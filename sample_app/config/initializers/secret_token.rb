f40462b53bb885792a9e5c28a9c1298dd62f4cec
Wan Yechao <wyc.jar@gmail.com>
2013-03-20 16:50:23 +0800

    add sample_app

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2b3488a4acf8634d556d68f4b69d71b9ef873ed7d64b13a1cc70b6e0e128b56ca2fd576e409e5f678e3e82c3cd736b2c7740d3ec8ba18b6910a4fe351287f149'
