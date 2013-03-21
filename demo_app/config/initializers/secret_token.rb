67559091d68c0ccaa36dd3f7f38d1c68ce01a829
Wan Yechao <wyc.jar@gmail.com>
2013-03-21 17:30:31 +0800

    delete no used files

--- demo_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '1fdebe9b59520fa494d9c58cf0cf354cdc869fda189a3a39363505a99c347842503b51f55dbf8b6e4df0107635e482dbd32babe5a32c033bf7aee7716e6ac018'
