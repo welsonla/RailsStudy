7e6b56286c76a3febc88ca801d096171f5ad1c96
Wan Yechao <wyc.jar@gmail.com>
2013-03-22 15:34:26 +0800

    add password to users

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    # @user = User.new(name: "Example User",
+    #                 email: "user@example.com",
+    #                 password: "foobar",
+    #                 password_confirmation: "foobar")
+    
   end
 end
