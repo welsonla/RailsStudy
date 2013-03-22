9e818bca61b6ea0b012ab45e741e0790b92679a3
Wan Yechao <wyc.jar@gmail.com>
2013-03-22 14:23:12 +0800

    add static routes,delete index.html

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  
+  match '/about' , to:"static_pages#about"
 
-  get "static_pages/help"
+  match '/contact',to:"static_pages#contact"
 
+  match '/home',   to: "static_pages#home"
+
+  match '/help',   to:"static_pages#help"
+
+
+  root :to =>  "static_pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
