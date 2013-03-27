ae93a3d746c4f4fe281b8744cd76dcdc24b0e9cd
Wan Yechao <wyc.jar@gmail.com>
2013-03-27 14:51:06 +0800

    Sign up

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   
+  #创建新用户界面
   def new
-    # @user = User.new(name: "Example User",
-    #                 email: "user@example.com",
-    #                 password: "foobar",
-    #                 password_confirmation: "foobar")
-    
+    @user = User.new
   end
   
+  #插入新用户
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Sample App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  #所有用户展示界面
   def show
     @user = User.find(params[:id])
   end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -9,6 +9,8 @@
 #  updated_at :datetime        not null
 #
 
+#add some comments
+
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest, :password, :password_confirmation 
   has_secure_password
